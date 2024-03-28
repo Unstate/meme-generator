fbd1e3a53c3d2c5fd9eff39a055b29d9666a8d07
Fetisov <rofetisov@mail.ru>
2024-03-28 18:42:02 +0300

    chore: Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+// Импорт React и ReactDOM
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
+// Импорт компонента HomePage из файла ./pages/HomePage
+import HomePage from './pages/HomePage'
+
+// Импорт главных стилей приложения из файла ./styles/main.scss
+import './styles/main.scss'
+
+// Используем метод createRoot() объекта ReactDOM, чтобы создать корневой компонент
+// и вызываем render(), чтобы отрисовать компонент в указанном месте на странице
+// Метод createRoot() и render() используются для активации экспериментального режима Concurrent Mode
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  // Оборачиваем компонент в <React.StrictMode>, чтобы выявить потенциальные проблемы в коде
   <React.StrictMode>
-    <App />
+    {/* Рендерим компонент HomePage */}
+    <HomePage />
   </React.StrictMode>,
 )
