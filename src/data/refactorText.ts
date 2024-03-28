d4708528d28b579d0ea4b715d643ec440ddbea1d
Fetisov <rofetisov@mail.ru>
2024-03-28 18:45:13 +0300

    chore: Add refactorText contant

---
+++ src/data/refactorText.ts
@@ -0,0 +1,58 @@
+export type TRefactors = typeof REFACTORS
+
+export const REFACTORS = [
+  {
+    title: 'underline',
+    image: './icons/underline.svg',
+    style: 'text-transform: underline',
+    active: false,
+  },
+  {
+    title: 'bold',
+    image: './icons/bold.svg',
+    style: 'font-weight: bold',
+    active: false,
+  },
+  {
+    title: 'italic',
+    image: './icons/italics.svg',
+    style: 'font-style: italic',
+    active: false,
+  },
+  {
+    title: 'red',
+    image: './icons/red.svg',
+    style: 'color: red',
+    active: false,
+  },
+  {
+    title: 'yellow',
+    image: './icons/yellow.svg',
+    style: 'color: yellow',
+    active: false,
+  },
+  {
+    title: 'green',
+    image: './icons/green.svg',
+    style: 'color: green',
+    active: false,
+  },
+  {
+    title: 'black',
+    image: './icons/black.svg',
+    style: 'color: black',
+    active: false,
+  },
+  {
+    title: '20px',
+    image: './icons/text.svg',
+    style: 'font-size: 14px',
+    active: false,
+  },
+  {
+    title: '30px',
+    image: './icons/text.svg',
+    style: 'font-size: 20px',
+    active: false,
+  },
+]
